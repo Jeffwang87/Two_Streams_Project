7640e88b2084c9385d239f4b651da287b8012753
Jianxin Wang <46662361+Jeffwang87@users.noreply.github.com>
2022-08-16 16:09:15 -0500

    Update spatial_stream_gpu.py

--- two-stream-pytorch-master/spatial_stream_gpu.py
+++ two-stream-pytorch-master/spatial_stream_gpu.py
@@ -17,10 +17,10 @@ import torchvision.transforms as transforms
 
 import models
 import datasets
-import load_HAA
-import load_HAA_Sorted
+import load_HAA_spatial_all
+#import load_HAA_Sorted
 import gc
-import GPUtil
+#import GPUtil
 
 
 #path = "/home/jupyter-arangion/two-stream-pytorch-master/datasets/HAA500_frames"
@@ -40,11 +40,11 @@ parser.add_argument('data', metavar='DIR',
 parser.add_argument('--dataset', '-d', default='HAA500',
                     choices=["ucf101", "hmdb51", "HAA500"],
                     help='dataset: ucf101 | hmdb51 | HAA500')
-parser.add_argument('--arch', '-a', metavar='ARCH', default='rgb_resnet152',
+parser.add_argument('--arch', '-a', metavar='ARCH', default='rgb_resnet18',
                     choices=model_names,
                     help='model architecture: ' +
                         ' | '.join(model_names) +
-                        ' (default: reg_resnet152)')
+                        ' (default: reg_resnet18)')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('--epochs', default=250, type=int, metavar='N',
@@ -53,7 +53,7 @@ parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=32, type=int,
                     metavar='N', help='mini-batch size (default: 50)')
-parser.add_argument('--iter-size', default=5, type=int,
+parser.add_argument('--iter-size', default=2, type=int,
                     metavar='I', help='iter size as in Caffe to reduce memory usage (default: 5)')
 parser.add_argument('--new_length', default=1, type=int,
                     metavar='N', help='length of sampled video frames (default: 1)')
@@ -61,7 +61,7 @@ parser.add_argument('--new_width', default=340, type=int,
                     metavar='N', help='resize width (default: 340)')
 parser.add_argument('--new_height', default=256, type=int,
                     metavar='N', help='resize height (default: 256)')
-parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
+parser.add_argument('--lr', '--learning-rate', default=0.0001, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--lr_steps', default=[100, 200], type=float, nargs="+",
                     metavar='LRSteps', help='epochs to decay learning rate by 10')
@@ -73,7 +73,7 @@ parser.add_argument('--print-freq', default=50, type=int,
                     metavar='N', help='print frequency (default: 50)')
 parser.add_argument('--save-freq', default=10, type=int,
                     metavar='N', help='save frequency (default: 25)')
-parser.add_argument('--resume', default='./two-stream-pytorch-master/checkpoints', type=str, 
+parser.add_argument('--resume', default='/mmfs1/data/wangccy/Two_Streams_Project/spatial_final_save_2/', type=str, 
                     metavar='PATH', help='path to latest checkpoint (default: none)')
 parser.add_argument('-e', '--evaluate', default=0, type=int, metavar='N',
                     help='evaluate model on validation set N times')
@@ -217,7 +217,7 @@ def main():
 
 def build_model():
 
-    model = models.__dict__['rgb_resnet152'](pretrained=True, num_classes=500)
+    model = models.__dict__['rgb_resnet18'](pretrained=True, num_classes=500)
     model.cuda()
     return model
 
@@ -240,7 +240,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
         target = data['label'].cuda(non_blocking=True)
         #print(input.size())
         #print(target.size())
-        input = torch.reshape(input, (32, 3, 224, 224))
+        #input = torch.reshape(input, (32, 3, 224, 224))
         #print(input.size())
         #print(target.size())
         input_var = torch.autograd.Variable(input)
